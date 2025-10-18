a5b68727cc0ade4050bc8e84a9efeb78d06e7dd2
josevitor1899 <josevitorfreitassena@gmail.com>
2025-10-18 19:10:59 -0300

    Produzindo o texto 2 da Página Inicial

--- src/components/TextosInicio/Texto02.vue
+++ src/components/TextosInicio/Texto02.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="Texto02">
     <h1>Dificuldades Enfrentadas pelos nossos clientes</h1>
+
+    <div id="DificuldadesEnfrentadas">
+        <img src="@/assets/imgsInicial/CadeiraDeRodas.jpg" alt="">
+        <img src="@/assets/imgsInicial/Psicologico.jpg" alt="">
+        <img src="@/assets/imgsInicial/FaltaDeVisibilidade.jpg" alt="">
+    </div>
   </div>
 </template>
 
@@ -12,4 +18,14 @@ export default {
 
 <style scoped>
 
+#DificuldadesEnfrentadas {
+    display: flex;
+}
+#DificuldadesEnfrentadas img{
+    width: 300px;
+    height: 250px;
+    margin-right: 20px;
+    border-radius: 7px;
+  
+}
 </style>
